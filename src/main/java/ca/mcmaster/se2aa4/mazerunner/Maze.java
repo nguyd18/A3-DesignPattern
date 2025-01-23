6051ccd145b415dc296ad9514d93ce7881d678be
Daniel Nguyen <nguyd18@mcmaster.ca>
2025-01-23 12:41:38 -0500

    initial walking skeleton

---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
@@ -0,0 +1,33 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+public class Maze {
+    private char[][] maze_array;
+
+    /**
+     * Load the maze from a file
+     */
+    public void loadMaze() {
+
+    }
+
+    /**
+     * Find the entrance of the maze
+     */
+    public void findEntry() {
+
+    }
+
+    /**
+     * Find the exit of the maze
+     */
+    public void findExit() {
+
+    }
+
+    /**
+     * Print the maze on to the terminal
+     */
+    public void printMaze() {
+
+    }
+}
---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/PathFinder.java
@@ -0,0 +1,26 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+public class PathFinder {
+    
+    /**
+     * Constructor method
+     */
+    public PathFinder() {
+
+    }
+
+    /**
+     * Check if the next cell is empty
+     */
+    public void canMoveForward() {
+
+    }
+
+    /**
+     * Generates the correct path of the maze (in canonical form)
+     */
+    public void generatePath() {
+
+    }
+
+}
