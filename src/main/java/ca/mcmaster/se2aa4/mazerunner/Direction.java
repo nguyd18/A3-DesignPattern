b643b034bd33bf5f4b5e343d28996722361a15c7
Daniel Nguyen <nguyd18@mcmaster.ca>
2025-02-06 19:26:56 -0500

    updated Direction to implement turnRight and turnLeft functions

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Direction.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Direction.java
@@ -2,4 +2,40 @@ package ca.mcmaster.se2aa4.mazerunner;
 
 public enum Direction {
     NORTH, EAST, SOUTH, WEST;
+
+    /**
+     * @return the direction that results from turning right
+     */
+    public Direction turnRight() {
+        switch (this) {
+            case NORTH:
+                return EAST;
+            case EAST:
+                return SOUTH;
+            case SOUTH:
+                return WEST;
+            case WEST:
+                return NORTH;
+            default:
+                return this;
+        }
+    }
+
+    /**
+     * @return the direction that results from turning left
+     */
+    public Direction turnLeft() {
+        switch (this) {
+            case NORTH:
+                return WEST;
+            case WEST:
+                return SOUTH;
+            case SOUTH:
+                return EAST;
+            case EAST:
+                return NORTH;
+            default:
+                return this;
+        }
+    }
 }
