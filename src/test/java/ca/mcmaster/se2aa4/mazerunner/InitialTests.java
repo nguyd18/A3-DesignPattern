99cfbf5b62e62507bcf52d252373f05d92a522c4
Daniel Nguyen <nguyd18@mcmaster.ca>
2025-04-03 20:38:00 -0400

    modified and added test cases to increase code coverage

--- src/test/java/ca/mcmaster/se2aa4/mazerunner/InitialTests.java
+++ src/test/java/ca/mcmaster/se2aa4/mazerunner/InitialTests.java
@@ -27,57 +27,83 @@ public class InitialTests {
     }
 
     /**
-     * Test to see if the path finder finds the correct path to the straight maze
+     * Test turning left
      */
     @Test
-    public void solveStraightMaze() {
-        MazeLoader loader = new MazeLoader();
-        Maze maze = loader.load("./examples/straight.maz.txt");
-        MazeSolver solver = new PathFinder(maze);
-        String expectedPath = "4F";
-        String actualPath = solver.findPath();
-        assertEquals(expectedPath, actualPath);
+    public void testTurnLeft() {
+        Direction direction = Direction.NORTH;
+        Direction expectedDirection = Direction.WEST;
+        Direction actualDirection = direction.turnLeft();
+        assertEquals(expectedDirection, actualDirection);
     }
 
     /**
-     * Test to see if the path validator will validate the correct path to the straight maze
+     * Test turning right
      */
     @Test
-    public void validateStraightMaze() {
-        MazeLoader loader = new MazeLoader();
-        Maze maze = loader.load("./examples/straight.maz.txt");
-        PathValidator validator = new PathValidator(maze);
-        String path = "4F";
-        String expectedValidation = "correct path";
-        String actualValidation = validator.validatePath(path);
-        assertEquals(expectedValidation, actualValidation);
+    public void testTurnRight() {
+        Direction direction = Direction.NORTH;
+        Direction expectedDirection = Direction.EAST;
+        Direction actualDirection = direction.turnRight();
+        assertEquals(expectedDirection, actualDirection);
     }
 
     /**
-     * Test to see if the path finder finds the correct path to the tiny maze
+     * Test to see if the wall is correctly identified
      */
     @Test
-    public void solveTinyMaze() {
-        MazeLoader loader = new MazeLoader();
-        Maze maze = loader.load("./examples/tiny.maz.txt");
-        MazeSolver solver = new PathFinder(maze);
-        String expectedPath = "5F 2L 2F R 2F R 2F 2L 2F R 2F R 3F";
-        String actualPath = solver.findPath();
-        assertEquals(expectedPath, actualPath);
+    public void testIsWall() {
+        Maze maze = new Maze();
+        maze.initialize(10, 10);
+        maze.fillRow(0, "##########");
+        maze.fillRow(1, "#        #");
+        maze.fillRow(2, "#        #");
+        maze.fillRow(3, "#        #");
+        maze.fillRow(4, "#        #");
+        maze.fillRow(5, "#        #");
+        maze.fillRow(6, "#        #");
+        maze.fillRow(7, "#        #");
+        maze.fillRow(8, "#        #");
+        maze.fillRow(9, "##########");
+
+        assertTrue(maze.isWall(0, 0), "Expected a wall at (0, 0)");
+        assertFalse(maze.isWall(1, 1), "Expected no wall at (1, 1)");
     }
 
     /**
-     * Test to see if the path validator will validate the correct path to the tiny maze
+     * Test to see if the maze loader loads the maze correctly
      */
     @Test
-    public void validateTinyMaze() {
+    public void testMazeLoader() {
         MazeLoader loader = new MazeLoader();
-        Maze maze = loader.load("./examples/tiny.maz.txt");
-        PathValidator validator = new PathValidator(maze);
-        String path = "5F 2L 2F R 2F R 2F 2L 2F R 2F R 3F";
-        String expectedValidation = "correct path";
-        String actualValidation = validator.validatePath(path);
-        assertEquals(expectedValidation, actualValidation);
+        Maze maze = loader.load("./examples/small.maz.txt");
+        assertNotNull(maze, "Maze should not be null");
+        assertFalse(maze.isWall(8, 0), "Entry should not be a wall");
+        assertFalse(maze.isWall(5, 10), "Exit should not be a wall");
+    }
+
+    /**
+     * Test to see if maze knows the start point is
+     */
+    @Test
+    public void testMazeStartPoint() {
+        MazeLoader loader = new MazeLoader();
+        Maze maze = loader.load("./examples/small.maz.txt");
+        int[] actual = maze.getEntry();
+        int[] expected = {8, 0};
+        assertArrayEquals(expected, actual, "The start point is incorrect");  
+    }
+
+    /**
+     * Test to see if maze knows the exit point is
+     */
+    @Test
+    public void testMazeExitPoint() {
+        MazeLoader loader = new MazeLoader();
+        Maze maze = loader.load("./examples/small.maz.txt");
+        int[] actual = maze.getExit();
+        int[] expected = {5, 10};
+        assertArrayEquals(expected, actual, "The exit point is incorrect");  
     }
 
     /**
